28dd13326398ff39cf9619185dfe98d2260d027a
Paras mehra <paras8223880607@gmail.com>
2026-07-17 12:58:18 +0530

    Update Students.py

--- pages/Students.py
+++ pages/Students.py
@@ -1,90 +1,59 @@
 import streamlit as st
 import pandas as pd
-import base64
 
 from database.mongodb import student_collection
 
 st.title("📋 Student List")
 
-search = st.text_input(
-    "Search Student"
-)
+search = st.text_input("Search Student")
 
 query = {}
 
 if search:
-
     query = {
-
-        "$or":[
-
+        "$or": [
             {
-                "first_name":{
-                    "$regex":search,
-                    "$options":"i"
+                "first_name": {
+                    "$regex": search,
+                    "$options": "i"
                 }
             },
-
             {
-                "email":{
-                    "$regex":search,
-                    "$options":"i"
+                "email": {
+                    "$regex": search,
+                    "$options": "i"
                 }
             }
         ]
     }
 
-students = list(
-    student_collection.find(query)
-)
+students = list(student_collection.find(query))
 
 if students:
 
-    for student in student:
+    for student in students:
 
         student["_id"] = str(student["_id"])
 
+        # Agar photo Base64 string me store hai
         if student.get("photo"):
-            student["photo"] = "data:image/Jpeg;base64," + base64.b64decode(student["photo"]).decode("latin")
+            student["photo"] = f"data:image/jpeg;base64,{student['photo']}"
         else:
             student["photo"] = ""
-    
 
-    df = pd.DataFrame(student)
+    df = pd.DataFrame(students)
 
-    img = JsCode("""
-    class ImgRenderer{
-        init(params){
-            this.eGui=document.createElement('img');
-            this.eGui.src=params.value;
-            this.eGui.width=60;
-            this.eGui.height=60;
-        }
-        getGui(){
-            return this.eGui;
-        }
-    }
-    """)
-
-    gb = GridOptionsBuilder.from_dataframe(df)
-
-    gb.configure_column(
-        "photo",
-        cellRenderer=img
-    )
-
-    gridOptions = gb.build()
-
-    AgGrid(
+    st.dataframe(
         df,
-        gridOptions=gridOptions,
-        allow_unsafe_jscode=True
+        column_config={
+            "photo": st.column_config.ImageColumn(
+                "Photo",
+                help="Student Photo"
+            )
+        },
+        hide_index=True,
+        use_container_width=True
     )
-
-
 
 else:
-
-    st.warning(
-        "No Students Found"
-    )
+    st.warning("No Students Found")
