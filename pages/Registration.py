78adc7e3642ff929f6664d19526848984e192990
Paras mehra <paras8223880607@gmail.com>
2026-08-25 12:20:16 +0530

    Update Registration.py

--- pages/Registration.py
+++ pages/Registration.py
@@ -2,38 +2,16 @@ import streamlit as st
 
 st.title("Student Registration")
 
-first_name = st.text_input(
-    "First Name"
-)
+first_name = st.text_input("First Name")
 
-last_name = st.text_input(
-    "Last Name"
-)
+last_name = st.text_input("Last Name")
 
-email = st.text_input(
-    "Email"
-)
+email = st.text_input("Email")
 
-course = st.text_input(
-    "Course"
-)
+course = st.text_input("Course")
 
-# Photo Upload
-photo = st.file_uploader(
-    "Upload Student Photo",
-    type=["jpg", "jpeg", "png"]
-)
-
-# Photo Preview
-if photo is not None:
-    st.image(photo, width=150, caption="Student Photo")
 
 if st.button("Register Student"):
-
-    photo_data = None
-
-    if photo is not None:
-        photo_data = photo.read()
 
     student_collection.insert_one({
 
@@ -43,9 +21,7 @@ if st.button("Register Student"):
 
         "email": email,
 
-        "course": course,
-
-        "photo": photo_data
+        "course": course
 
     })
 
